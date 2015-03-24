b901e81db875c0993fbef32fb426a25b24cb5b78
Kevin Aung <kevin.aung@yahoo.com>
2015-03-24 15:32:14 -0700

    cust disp takes incoming html and renders on view. Took out scripts for faster load

--- routes/display.js
+++ routes/display.js
@@ -6,25 +6,23 @@ var express = require('express');
 var router = express.Router();
 var port = 3001;
 
-var check = new Check();
-/*
-check.items.push({ name: 'Firecracker Shrimp', value: 5.99 });
-check.items.push({ name: 'Seabreeze Sunrise', value: 10.05 });
-check.taxes.push({ name: 'Sales Tax', value: 2.99 });
-check.taxes.push({ name: 'Event Tax', value: 3.99 });
-check.subtotal = 15.99;
-check.total = 22.97;
-*/
+//var model = {
+//    html = '<a href="#">Link</a>',
+//    jquery = true,
+//    jqueryui = false,
+//    bootstrap = true,
+//    bootstrapscript = false
+//};
+var model = {};
 
 // GET
 router.get('/', function(req, res, _) {
-    res.render('display', { check: check });
+    res.render('display', model);
 });
 
 // POST
 router.post('/', function(req, res, _) {
-    console.log(JSON.stringify(req.body));
-    check = req.body;
+    model = req.body;
     client.send('refresh');
     res.writeHead(202, { 'Content-Type': 'plain/text' });
     res.end();
@@ -35,26 +33,6 @@ var server = new WebSocketServer({ port: port });
 server.on('connection', function(socket) {
     console.log('client connected');
     client = socket;
-    //socket.send(JSON.stringify(check));
 });
-
-
-// Sockets!
-//var server = net.createServer(function(socket) {
-//    console.log('client connected!');
-//    socket.on('end', function() {
-//        console.log('client disconnected');
-//    });
-//
-//    socket.write(JSON.stringify(check));
-//});
-//
-//server.on('error', function(err) {
-//    console.error(err);
-//});
-//
-//server.listen(port, function() {
-//    console.log('customer display server listening on port: ' + port);
-//});
 
 module.exports = router;
